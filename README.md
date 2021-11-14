d81e5e59456ee3080a160cdec0390f40240087fb
lisy0123 <leesese3320@gmail.com>
2021-11-14 23:17:07 +0900

    create greeting, clock, quotes, todolist

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Momentum Clone
 
-Momentum Clone made with VaillaJS
+Momentum Clone made with JS
 
 [↩️ Go Back](https://github.com/lisy0123/Nomadcoders)
 
---
+++ js/todo.js
@@ -0,0 +1,58 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement.parentElement;
+    li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    const spanDelete = document.createElement("span");
+
+    span.classList.add("item");
+    spanDelete.classList.add("delete");
+    span.innerText = newTodo.text;
+    spanDelete.innerHTML = `<i class="far fa-trash-alt"></i>`;
+
+    li.appendChild(span);
+    li.appendChild(spanDelete);
+    toDoList.appendChild(li);
+    spanDelete.addEventListener("click", deleteToDo);
+}
+
+function handleToDoSummit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSummit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos != null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
